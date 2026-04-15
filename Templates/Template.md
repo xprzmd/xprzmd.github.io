265d8c21757ccdbdaa9544e999057684baeadd04
xprzmd <xprzmd@gmail.com>
2026-04-15 19:40:26 +0800

    vault backup: 2026-04-15 19:40:26

---
+++ Templates/Template.md
@@ -0,0 +1,11 @@
+---
+author: xprzmd
+pubDatetime: "{{date:YYYY-MM-DD}}"
+modDatetime:
+title:
+slug:
+featured: false
+draft: false
+tags:
+description: a template
+---
--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
   website: "https://xprzmd.site", // replace this with your deployed domain
   author: "xprzmd",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "MyBlog",
-  ogImage: "astropaper-og.jpg",
+  profile: "https://xprzmd.site",
+  desc: "blog for sharing knowledge and experiences.",
+  title: "Xprzmd's Blog",
+  ogImage: "wallhaven-qrggkd.png",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
@@ -14,10 +14,10 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/xprzmd/xprzmd.github.io/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "zh-CN", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
